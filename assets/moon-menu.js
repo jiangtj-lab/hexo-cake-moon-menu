295c8e1f75530bc2c4fe71e3c03a9dc8fb368df1
jiangtj <116749895@qq.com>
2020-04-21 11:19:19 +0800

    fix function

--- assets/moon-menu.js
+++ assets/moon-menu.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-var */
 /* global document,window */
 'use strict';
 ((window, document) => {
@@ -32,43 +33,43 @@
   });
   window.addEventListener('scroll', moonMenuListener);
 
-  // eslint-disable-next-line no-unused-vars
-  const moonMenuClick = function() {
-    const items = document.querySelector('.moon-menu-items');
-    items.classList.toggle('active');
-    const points = document.querySelectorAll('.moon-menu-point');
-    const childItems = document.querySelectorAll('.moon-menu-item');
-    if (items.classList.contains('active')) {
-      points[0].setAttribute('cx', '-.8rem');
-      points[0].setAttribute('cy', '0');
-      points[2].setAttribute('cx', '.8rem');
-      points[2].setAttribute('cy', '0');
-      for (let i = 0; i < childItems.length; i++) {
-        childItems[i].style.top = -3 - 3 * i + 'rem';
-        childItems[i].style.opacity = .9;
-      }
-    } else {
-      points[0].setAttribute('cx', '0');
-      points[0].setAttribute('cy', '-.8rem');
-      points[2].setAttribute('cx', '0');
-      points[2].setAttribute('cy', '.8rem');
-      for (let i = 0; i < childItems.length; i++) {
-        childItems[i].style.top = '1rem';
-        childItems[i].style.opacity = 0;
-      }
-    }
-  };
-
-  // eslint-disable-next-line no-unused-vars
-  const back2top = () => {
-    window.scroll({ top: 0, behavior: 'smooth' });
-  };
-
-  // eslint-disable-next-line no-unused-vars
-  const back2bottom = () => {
-    const offsetHeight = document.documentElement.offsetHeight;
-    const scrollHeight = document.documentElement.scrollHeight;
-    window.scroll({ top: scrollHeight - offsetHeight, behavior: 'smooth' });
-  };
-
 })(window, document);
+
+// eslint-disable-next-line no-unused-vars
+var moonMenuClick = function() {
+  const items = document.querySelector('.moon-menu-items');
+  items.classList.toggle('active');
+  const points = document.querySelectorAll('.moon-menu-point');
+  const childItems = document.querySelectorAll('.moon-menu-item');
+  if (items.classList.contains('active')) {
+    points[0].setAttribute('cx', '-.8rem');
+    points[0].setAttribute('cy', '0');
+    points[2].setAttribute('cx', '.8rem');
+    points[2].setAttribute('cy', '0');
+    for (let i = 0; i < childItems.length; i++) {
+      childItems[i].style.top = -3 - 3 * i + 'rem';
+      childItems[i].style.opacity = .9;
+    }
+  } else {
+    points[0].setAttribute('cx', '0');
+    points[0].setAttribute('cy', '-.8rem');
+    points[2].setAttribute('cx', '0');
+    points[2].setAttribute('cy', '.8rem');
+    for (let i = 0; i < childItems.length; i++) {
+      childItems[i].style.top = '1rem';
+      childItems[i].style.opacity = 0;
+    }
+  }
+};
+
+// eslint-disable-next-line no-unused-vars
+var back2top = () => {
+  window.scroll({ top: 0, behavior: 'smooth' });
+};
+
+// eslint-disable-next-line no-unused-vars
+var back2bottom = () => {
+  const offsetHeight = document.documentElement.offsetHeight;
+  const scrollHeight = document.documentElement.scrollHeight;
+  window.scroll({ top: scrollHeight - offsetHeight, behavior: 'smooth' });
+};
